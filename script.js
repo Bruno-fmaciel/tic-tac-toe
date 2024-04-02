c1752fef8e3bf4a7caa249e4cc1cfac1e5f8626e
Bruno-fmaciel <bfma@cesar.school>
2024-04-02 09:26:40 -0300

    Add logic for win and draw

--- script.js
+++ script.js
@@ -75,11 +75,62 @@ playerTwoName = "Player Two") {
         console.log(`${getActivePlayer().name}'s turn`);
     }
 
+    const checkWinner = () => {
+        const board = gameboard.getBoard();
+
+        for (let i=0;i<board.length;i++) {
+            if (board[i][0].getValue() !== 0 && board[i][0].getValue() === board[i][1].getValue() && board[i][0].getValue() === board[i][2].getValue()) {
+                return players.find(player => player.token === board[i][0].getValue());
+            };
+        }
+
+        for (let i=0;i<board[0].length;i++) {
+            if (board[0][i].getValue() !== 0 && board[0][i].getValue() === board[1][i].getValue() && board[0][i].getValue() === board[2][i].getValue()) {
+                return players.find(player => player.token === board[0][i].getValue());
+            };
+        }
+
+        if (board[0][0].getValue() !== 0 && board[0][0].getValue() === board[1][1].getValue() && board[0][0].getValue() === board[2][2].getValue()) {
+            return players.find(player=> player.token === board[0][0].getValue());
+        }
+
+        if (board[0][2].getValue() !== 0 && board[0][2].getValue() === board[1][1].getValue() && board[0][2].getValue() === board[2][0].getValue()) {
+            return players.find(player=> player.token === board[0][2].getValue());
+        }
+
+        return null;
+    }
+
+    const itsDraw = () => {
+        const board = gameboard.getBoard();
+
+        for (let i = 0; i<board.length; i++){
+            for(let j=0; j<board[i].length; j++){
+                if (board[i][j].getValue() === 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     const playRound = (row,column) => {
         console.log(`Marking ${getActivePlayer().token} for ${getActivePlayer().name}, into row ${row} and column ${column}.`);
         gameboard.mark(row,column,getActivePlayer().token);
 
         //Place logic for cheking if there's a winner.
+        const winner = checkWinner();
+        if (winner) {
+            console.log(`${winner.name} has won!`)
+            return;
+        }
+
+        if (itsDraw()) {
+            console.log("It's a draw!");
+            return;
+        }
+
 
         switchPlayerTurn();
         printNewRound();
