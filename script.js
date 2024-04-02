28ad1ef460168f55ca6155bbcaeb0017e28ca088
Bruno-fmaciel <bfma@cesar.school>
2024-04-02 08:04:42 -0300

    Add console game logic (needs fixing)

---
+++ script.js
@@ -0,0 +1,96 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const gameboard = [];
+
+    for (let i = 0; i < rows; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < columns; j++) {
+            gameboard[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => gameboard;
+
+    const mark = (row,column,player) => {
+        if (gameboard[row][column] === 0) {
+            gameboard[row][column].addMark(player);
+        } else {
+            return;
+        }
+    }
+
+    const printBoard = () => {
+        const boardWithCellValues = gameboard.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+    
+    return {
+        getBoard,
+        mark,
+        printBoard
+    }
+}
+
+function Cell() {
+    let value = 0;
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    }
+}
+
+/*function Player(name) {
+    return {
+        name
+    };
+}*/
+
+function GameController(playerOneName = "Player One",
+playerTwoName = "Player Two") {
+    const gameboard = Gameboard();
+
+    const players = [
+        {name : playerOneName,
+        token : 1},
+        {name : playerTwoName,
+        token : 2}
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => activePlayer = activePlayer === players[0]? players[1] : players[0];
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        gameboard.printBoard();
+        console.log(`${getActivePlayer().name}'s turn`);
+    }
+
+    const playRound = (row,column) => {
+        console.log(`Marking ${getActivePlayer().token} for ${getActivePlayer().name}, into row ${row} and column ${column}.`);
+        gameboard.mark(row,column,getActivePlayer().token);
+
+        //Place logic for cheking if there's a winner.
+
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    }
+}
+
+const game = GameController();
